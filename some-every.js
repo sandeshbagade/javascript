adb8b5e1a9c378ebc5be37d664f25eced9b65116
sandeshbagade <sandeshbagade25@gmail.com>
2021-07-01 23:38:38 +0530

    Javascript array methods which look similar but are built for different use case

--- some-every.js
+++
@@ -1,9 +0,0 @@
-//Two underrated array methods that you must know .some() .every()
-const array = [32, 65, 32, 89, 45]
-
-function check(v){
-    return v<35
-}
-
-console.log(array.some(check)) //at least one item of array should satisfy the condition
-console.log(array.every(check))//every item of array should satisfy the condition
